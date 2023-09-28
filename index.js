4e7119323a97092e8bca39832e144058c7b043bb
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-09-28 08:28:16 -0600

    add boiler plate, add quit functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2/promise");
+
+const mainMenu = ["Quit"];
+
+async function main() {
+  const db = await mysql.createConnection({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+  let active = true;
+  while (active) {
+    const response = await inquirer.prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: mainMenu,
+      },
+    ]);
+    switch (response.action) {
+      case "Quit":
+        active = false;
+        break;
+    }
+  }
+  process.exit();
+}
